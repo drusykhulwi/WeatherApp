55b587e2b604c8c854287567e5ed3954d668fc2e
drusykhulwi <drusillakhulwael@gmail.com>
2024-03-12 13:09:53 +0300

    API Integration

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,8 +1,23 @@
+function updateWeather(response) {
+  let tempElement = document.querySelector("#temp-value");
+  let theTemp = response.data.temperature.current;
+  tempElement.innerHTML = Math.round(theTemp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+function searchCity(city) {
+  let apiKey = "a39to9848ebf1032a3bb6a0b1f2641cd";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchBarElement = document.querySelector("#search-bar");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchBarElement.value;
+
+  searchCity(searchBarElement.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
+
+searchCity("Miami");
